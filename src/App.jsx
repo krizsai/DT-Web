f11c9ddfe617dd4136b3acd632407f626836ed85
krizsai <saikrishnamuthukuru.1919@gmail.com>
2026-06-08 15:54:30 +0530

    Tailwindcss and daisy UI

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,15 @@ function App() {
   return (
     <>
       <h1>Hello World</h1>
+
+      <div className="flex flex-col items-center justify-center min-h-screen bg-base-200 gap-4">
+        <h1 className="text-4xl font-bold text-primary">
+          Tailwind + daisyUI are Live!
+        </h1>
+        <button className="btn btn-accent">daisyUI Button</button>
+      </div>
     </>
-  )
+  );
 }
 
 export default App
