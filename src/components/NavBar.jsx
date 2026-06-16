3751f6f68aa21195d6ded279fd65cccbe163e89d
krizsai <saikrishnamuthukuru.1919@gmail.com>
2026-06-16 08:23:00 +0530

    User state management for logout

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,22 +1,31 @@
 import { useSelector, useDispatch } from "react-redux";
 import { removeUser } from "../utils/userSlice";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
+import { BASE_URL } from "../utils/constants";
 
 const NavBar = () => {
   const user = useSelector((state) => state.userSlice);
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
-  const handleLogout = () => {
-    dispatch(removeUser());
-    navigate("/login");
+  const handleLogout = async () => {
+    try{
+      await axios.post(
+        BASE_URL + "/logout", {}, {withCredentials: true}
+      );
+      dispatch(removeUser());
+      return navigate("/login"); 
+    }catch(err){
+      console.log(err);
+    }
   };
 
   return (
     <div>
       <div className="navbar bg-base-300 shadow-sm">
         <div className="flex-1">
-          <a className="btn btn-ghost text-xl">💻 DevTinder</a>
+          <Link to="/" className="btn btn-ghost text-xl">💻 DevTinder</Link>
         </div>
         <div className="flex gap-2 display-flex">
           {user && (
@@ -41,13 +50,13 @@ const NavBar = () => {
                 className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
               >
                 <li>
-                  <a onClick={() => navigate("/profile")}>
+                  <Link to="/profile">
                     Profile
                     <span className="badge">New</span>
-                  </a>
+                  </Link>
                 </li>
                 <li>
-                  <a onClick={() => navigate("/settings")}>Settings</a>
+                  <Link to = "/settings">Settings</Link>
                 </li>
                 <li>
                   <a onClick={handleLogout}>Logout</a>
---
+++ src/utils/constants.js
@@ -0,0 +1,6 @@
+export const BASE_URL = "http://localhost:7777";
+
+export const ENDPOINTS = {
+  LOGIN: "/login",
+  PROFILE_VIEW: "/profile/view",
+};
