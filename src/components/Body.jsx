59bcd9130cce075f2b110d176be0ad49ce201c8e
krizsai <saikrishnamuthukuru.1919@gmail.com>
2026-06-16 08:23:12 +0530

    feat: implement user authentication check and profile data fetching in Body component

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,8 +1,37 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
 import NavBar from "./NavBar";
 import Footer from "./Footer";
+import { useDispatch, useSelector } from "react-redux";
+import { BASE_URL, ENDPOINTS } from "../utils/constants";
+import { useEffect } from "react";
+import axios from "axios";
+import { addUser } from "../utils/userSlice";
 
 const Body = () => {
+
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const userData = useSelector((state) => state.userSlice);
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      if (userData) return;
+      try {
+        const res = await axios.get(BASE_URL + ENDPOINTS.PROFILE_VIEW, {
+          withCredentials: true,
+        });
+        dispatch(addUser(res.data));
+      } catch (err) {
+        if (err.status === 401) {
+          navigate("/login");
+        }
+        console.log(err);
+      }
+    };
+
+    fetchUser();
+  }, [userData, dispatch, navigate]);
+
   return (
     <div>
       <NavBar />
